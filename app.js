31aad205b17819918355658984435f64d73645d5
riz1-lv <riz.shkh1@gmail.com>
2020-11-01 17:10:20 -0500

    basic numeral translation

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-function sum() {
-  
-  
-  let result = parseInt(arabicInput) + parseInt(romanInput);
-  
-}
+
 function romanify(){
   let arabicInput = document.getElementById('arabicInput').value;
   let result = parseInt(arabicInput);
@@ -20,15 +15,72 @@ function romanify(){
     $('#errorMsg').text('');
   }
 }
-
 function numerify(){
   let romanInput = document.getElementById('romanInput').value;
+  let result = romanInput;
+  if (!isNaN(result)) {
+    document.getElementById('arabicInput').value = '';
+    $('#romanErrorMsg').text('Enter a Roman numeral');
+    }
+    
+  if(isNaN(result)){
+    document.getElementById('arabicInput').value = arabicConvert(result);
+    $('#romanErrorMsg').text('');
+  }
+  if(romanInput.length === 0){
+    $('#romanErrorMsg').text('');
+  }
 }
 
-function romanConvert(num){
-  let decimalValue = [10000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+function arabicConvert(str){
+  let decimalValue = [1000000,500000,100000,50000, 10000, 5000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   let romanNumeral = [
+    '_M',
+    '_D',
+    '_C',
+    '_L',
     '_X',
+    '_V',
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+];
+
+  let numeral = 0;
+
+  for (let i = 0; i < decimalValue.length; i++) {
+    while (str.includes(romanNumeral[i])) {
+      numeral += decimalValue[i];
+      console.log(str);
+      str = str.replace(romanNumeral[i], "")
+      console.log(str);
+    }
+  }
+
+  return numeral;
+}
+
+
+function romanConvert(num){
+  let decimalValue = [1000000,500000,100000,50000, 10000, 5000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let romanNumeral = [
+    '_M',
+    '_D',
+    '_C',
+    '_L',
+    '_X',
+    '_V',
     "M",
     "CM",
     "D",
